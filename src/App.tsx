1340bc2351899d1e44174d93664befd30eacc81d
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-07 18:46:26 +0200

    setup redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { RecoilRoot } from "recoil";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "stores/store";
 import { Header, Content, Footer } from "components/+Layout";
 
 import "./App.css";
 
 function App() {
 	return (
-		<RecoilRoot>
-			<Router>
-				<Header />
-				<Content />
-				<Footer />
-			</Router>
-		</RecoilRoot>
+		<Provider store={store}>
+			<div className='App'>
+				<Router>
+					<Header />
+					<Content />
+					<Footer />
+				</Router>
+			</div>
+		</Provider>
 	);
 }
 
